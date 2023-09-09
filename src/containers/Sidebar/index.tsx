8f9b97e4bbb57683f0c0117cb7427f6125796b4c
Douglas Eduardo <douglas.patusco@gmail.com>
2023-09-09 20:03:00 -0300

    construindo a estrutura e aseção "sobre mim"

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,12 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => {
+  return (
+    <aside>
+      <img src="http://github.com/eyelexx.png" />
+      <Titulo fontSize={20}>Douglas Eduardo</Titulo>
+    </aside>
+  )
+}
+
+export default Sidebar
