d7a1e432faaaa1e4e05babef3da17ba29748fb4d
Douglas Eduardo <douglas.patusco@gmail.com>
2023-09-12 10:33:31 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,12 +1,23 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
 
-const Sidebar = () => {
-  return (
-    <aside>
-      <img src="http://github.com/eyelexx.png" />
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
+
+const Sidebar = () => (
+  <aside>
+    <SidebarContainer>
+      <Avatar />
       <Titulo fontSize={20}>Douglas Eduardo</Titulo>
-    </aside>
-  )
-}
+      <Paragrafo tipo="secundario" fontSize={16}>
+        eyelexx
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedor Front-End
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
+  </aside>
+)
 
 export default Sidebar
